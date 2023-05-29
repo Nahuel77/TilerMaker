0a7d30195a0eaf4b2b37ba10e4427a4a7ad7f71b
Nahuel <70348604+Nahuel77@users.noreply.github.com>
2023-05-29 11:53:22 -0300

    Add files via upload

--- atil/imports.py
+++ atil/imports.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import colorchooser
+from tkinter import colorchooser, ttk, filedialog
 from PIL import ImageTk, Image, ImageDraw, ImageGrab, ImageOps
 import queue as Q
 import numpy as np
--- atil/main.py
+++ atil/main.py
@@ -5,9 +5,8 @@ import imports
 ############################################################################################################
 
 root = imports.tk.Tk()
-root.geometry("800x600+50+50")
+root.geometry("1250x650+0+0")
 root.config(bg="#5C5C8C")
-root.wm_state('zoomed')
 root.resizable(False, False)
 color = "#000000"
 
@@ -73,9 +72,6 @@ def draw(event):
 			root.config(cursor="arrow")
 			draw_status = True
 
-def cancel_tool(event):
-	pass
-
 def seleccion_color():
 	global color
 	picked_color = imports.colorchooser.askcolor()
@@ -126,28 +122,37 @@ def update_mosaico_muestra():
 def call_constructor():
 	imports.constructor.construir(can_width, can_height, celdas, celdas2, constructor_mark)
 
+def guardar():
+	tilemap = imports.Image.open("background_tile.png")
+	archivo = imports.filedialog.asksaveasfilename(
+		defaultextension=".png",
+		filetypes=[("PNG", ".png")]
+		)
+	if archivo:
+		tilemap.save(archivo)
+
 ############################################################################################################
 
-pen_image = imports.Image.open("pen.png")
-pen_icon = pen_image.resize((20, 20))
+pen_img = imports.Image.open("pen.png")
+pen_icon = pen_img.resize((20, 20))
 pen_icon_tk = imports.ImageTk.PhotoImage(pen_icon)
 btn_pen = imports.tk.Button(root, image=pen_icon_tk, command=pen, bg="#7BC67B")
 btn_pen.place(x=20, y=40)
 
-color_imagen = imports.Image.open("paleta.png")
-color_icon = color_imagen.resize((20, 20))
+color_img = imports.Image.open("paleta.png")
+color_icon = color_img.resize((20, 20))
 color_icon_tk = imports.ImageTk.PhotoImage(color_icon)
 btn_color = imports.tk.Button(root, image=color_icon_tk, command=seleccion_color, bg="#7BC67B")
 btn_color.place(x=20, y=70)
 
-fill_image = imports.Image.open("fill.png")
-fill_icon = fill_image.resize((20, 20))
+fill_img = imports.Image.open("fill.png")
+fill_icon = fill_img.resize((20, 20))
 fill_icon_tk = imports.ImageTk.PhotoImage(fill_icon)
 btn_fill = imports.tk.Button(root, image=fill_icon_tk, command=fill, bg="#7BC67B")
 btn_fill.place(x=50, y=40)
 
-eyerdropper_image = imports.Image.open("gotero.png")
-eyerdropper_icon = eyerdropper_image.resize((20, 20))
+eyerdropper_img = imports.Image.open("gotero.png")
+eyerdropper_icon = eyerdropper_img.resize((20, 20))
 eyerdropper_icon_tk = imports.ImageTk.PhotoImage(eyerdropper_icon)
 btn_eyerdropper = imports.tk.Button(root, image=eyerdropper_icon_tk, command=eyerdropper, bg="#7BC67B")
 btn_eyerdropper.place(x=80, y=70)
@@ -155,23 +160,29 @@ btn_eyerdropper.place(x=80, y=70)
 frame = imports.tk.Frame(root, bg=color, width=26, height=26)
 frame.place(x=50, y=70)
 
-llevar_muestra_image = imports.Image.open("play.png")
-llevar_muestra_icon = llevar_muestra_image.resize((50, 50))
+llevar_muestra_img = imports.Image.open("play.png")
+llevar_muestra_icon = llevar_muestra_img.resize((50, 50))
 llevar_muestra_icon_tk = imports.ImageTk.PhotoImage(llevar_muestra_icon)
-btn_guardar = imports.tk.Button(root, image=llevar_muestra_icon_tk, command=update_mosaico_muestra, bg="#7BC67B")
-btn_guardar.place(x=820, y=40)
+btn_mostrar = imports.tk.Button(root, image=llevar_muestra_icon_tk, command=update_mosaico_muestra, bg="#7BC67B")
+btn_mostrar.place(x=820, y=40)
 
-clear_image = imports.Image.open("clear.png")
-clear_icon = clear_image.resize((20, 20))
+clear_img = imports.Image.open("clear.png")
+clear_icon = clear_img.resize((20, 20))
 clear_icon_tk = imports.ImageTk.PhotoImage(clear_icon)
 btn_clear = imports.tk.Button(root, image=clear_icon_tk, command=clear, bg="#7BC67B")
 btn_clear.place(x=80, y=40)
 
-constructor_image = imports.Image.open("constructor.png")
-constructor_icon = constructor_image.resize((50, 50))
+constructor_img = imports.Image.open("constructor.png")
+constructor_icon = constructor_img.resize((50, 50))
 constructor_icon_tk = imports.ImageTk.PhotoImage(constructor_icon)
 btn_constructor = imports.tk.Button(root, image=constructor_icon_tk, command=call_constructor, bg="#7BC67B")
-btn_constructor.place(x=20, y=480)
+btn_constructor.place(x=20, y=410)
+
+guardar_img = imports.Image.open("save.png")
+guardar_icon = guardar_img.resize((50, 50))
+guardar_icon_tk = imports.ImageTk.PhotoImage(guardar_icon)
+btn_guardar = imports.tk.Button(root, image=guardar_icon_tk, command=guardar, bg="#7BC67B")
+btn_guardar.place(x=20, y=510)
 
 mosaico_muestra = imports.tk.Canvas(root, width=can_width*pixel_size, height=can_height*pixel_size, highlightthickness=0)
 mosaico_muestra.place(x=900, y=40)
@@ -208,10 +219,26 @@ for i in range(can_width):
 label_clean = imports.tk.Label(root, width=20, height=20, image=clear_icon_tk, bg="#5C5C8C")
 label_clean.place_forget()
 
-label_constructor = imports.tk.Label(root, text="Blob", bg="#5C5C8C", font=("Arial", 12))
-label_constructor.place(x=140, y=370)
+tope_detail = imports.tk.Frame(root, bg="#2C2C4C", height=2, bd=0)
+tope_detail.place(x=800, y=120, width=76)
+
+separator_h = imports.tk.Frame(root, bg="#2C2C4C", height=2, bd=0)
+separator_h.place(x=0, y=380, width=(root.winfo_screenwidth()))
+
+label_constructor = imports.tk.Label(root, text="Tiles", bg="#5C5C8C", font=("Arial", 12))
+label_constructor.place(x=140, y=410)
 
 constructor_mark = imports.tk.Label(root, width=(can_width*11), height=(can_height*5), bg="#5C5C8C")
-constructor_mark.place(x=140, y=400)
+constructor_mark.place(x=140, y=450)
+
+separator_v = imports.tk.Frame(root, bg="#2C2C4C", width=2, bd=0)
+separator_v.place(x=820, y=380, height=300)
+
+msg =imports.tk.Text(root, bg="#5C5C8C", width=50, height=30 ,bd=0, font=("Arial", 12, "bold"))
+msg.insert(imports.tk.END, "Este programa ha sido desarrollado por\nNahuel Astor (nahuelastor@gmail.com).\n\
+Su propósito es ofrecer una solución sencilla\ny básica para el diseño de tilemaps.\n\
+Se distribuye de manera gratuita\ny no se autoriza su venta.\n\
+https://github.com/Nahuel77")
+msg.place(x=840, y=400)
 
 root.mainloop()
